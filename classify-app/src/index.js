0adcb2451d8300196c80cb38493481cedfc9250c
Kelvin Akyea <73767888+khelvyn80@users.noreply.github.com>
2023-04-24 21:32:14 +0000

    New changes to the route

--- classify-app/src/index.js
+++ classify-app/src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-=======
 // import { BrowserRouter } from 'react-router-dom';
->>>>>>> bdca2217d32c16d3484bff26bcb68462dcc4886d
 import './index.css';
 import App from './App';
 
